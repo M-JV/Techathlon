6b184ea360e44481a2864fb8b03ae777e3d4d950
M-JV <melvinjohn2001@gmail.com>
2023-09-15 11:39:21 +0530

    Project Initiation

--- moneyFy/transactions/views.py
+++ moneyFy/transactions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def transactions(request):
+    return render(request, 'transactions/transactions.html')
+
+
